65f73b1a2573f3a1da992cef76627ce07e58e905
Олег Білостоцький <oleg220298d@gmail.com>
2022-11-14 20:36:06 +0200

    changed hw4 and added hw5

--- Homework-4/script/main.js
+++ Homework-4/script/main.js
@@ -3,6 +3,8 @@ const createTask = document.querySelector('input');
 const buttonAdd = document.querySelector('.button');
 const buttonSortByTime = document.querySelector('.btn-sort-time');
 const buttonSortByAlphabet = document.querySelector('.btn-sort_A-Z');
+let tasks = document.querySelectorAll('#drag')
+let dragged = null;
 let toDoList = [];
 
 buttonAdd.addEventListener('click', () => {
@@ -20,13 +22,14 @@ const addToDo = (content, data) => {
     const time = document.createElement('time');
     const buttonDel = document.createElement('button');
     const buttonDone = document.createElement('button');
-    const randomId = (Math.random() * 123.45).toFixed(2);
-    const objId = randomId
 
     if (createTask.value === '') return    // Якщо інпут пустий, то не створювати таску
 
+    const randomId = (Math.random() * 123.45).toFixed(2);
     buttonDone.id = randomId
     buttonDel.id = randomId
+    p.id = randomId
+    const objId = randomId
 
     time.textContent = data;
     p.textContent = content;
@@ -34,6 +37,7 @@ const addToDo = (content, data) => {
     buttonDel.textContent = 'Del';
     buttonDone.textContent = 'Done';
 
+    div.id = 'drag';
     div.className = 'div-task';
     div1.className = 'div-time';
     div2.className = 'task-btn_btn';
@@ -43,19 +47,17 @@ const addToDo = (content, data) => {
     buttonDone.className = 'btn-done';
 
     div.setAttribute('draggable', 'true')
-    p.setAttribute('contenteditable', 'true');
-
-    containerTask.prepend(div);
 
     div.append(div1, div2);
     div1.prepend(p, time);
     div2.prepend(buttonDone, buttonDel);
 
+    containerTask.prepend(div);
+
     const createItemObj = (arr) => {
         const itemObj = {};
         itemObj.name = createTask.value;
         itemObj.id = objId;
-        itemObj.id2 = buttonDel.id
         itemObj.time = time.textContent;
         itemObj.done = false;
 
@@ -65,13 +67,40 @@ const addToDo = (content, data) => {
     createItemObj(toDoList)
 
     ButtonDoneAndDel(div, div2, p, buttonDel, buttonDone);
-    dragAndDrop();
+    dragAndDrop()
+    redactTask(p)
+}
+
+function redactTask(p) {
+    let paragraph = document.querySelectorAll('.p-task')
+
+    for (const paragraphElement of paragraph) {
+        paragraphElement.addEventListener('dblclick', function () {
+        let text = this.textContent;
+        this.textContent = '';
+
+        let edit = document.createElement('input');
+        edit.className = 'container-input'
+        edit.value = text;
+        this.appendChild(edit);
+
+        let self = this;
+        edit.addEventListener('keypress', function (event) {
+            if (event.key === 'Enter') {
+                self.textContent = this.value;
+                for (let i = 0; i < toDoList.length; i++) {
+                    if (p.id === toDoList[i].id) {
+                        toDoList[i].name = this.value
+                    }
+                }
+            }
+        });
+        });
+    }
 }
 
 function ButtonDoneAndDel(div, div2, p, buttonDel, buttonDone) {
     buttonDone.addEventListener('click', () => {
-
-        // Записую чи зроблена таска, чи ні
         for (let i = 0; i < toDoList.length; i++) {
             if (buttonDone.id === toDoList[i].id) {
                 toDoList[i].done = true
@@ -85,10 +114,8 @@ function ButtonDoneAndDel(div, div2, p, buttonDel, buttonDone) {
         buttonDone.className = 'btn-done-silver';
     })
 
-    // Видалення такси на сторінці та в об'єкті по ID
     buttonDel.addEventListener('click', () => {
         for (let i = 0; i < toDoList.length; i++) {
-            console.log(2)
             if (buttonDel.id === toDoList[i].id) {
                 toDoList.splice(i, 1);
                 div.remove();
@@ -108,7 +135,7 @@ const sortByAlphabetAndData = (time, data, id, done) => {
 
     newButtonDel.id = id
     newButtonDone.id = id
-
+    newP.id = id
     newTime.textContent = time;
     newP.textContent = data;
 
@@ -118,15 +145,14 @@ const sortByAlphabetAndData = (time, data, id, done) => {
     if (done) {
         newDiv.className = 'div-task-done';
         newP.className = 'p-task-done';
-        newP.setAttribute('contenteditable', 'false');
         newButtonDone.className = 'btn-done-silver';
     } else {
         newDiv.className = 'div-task';
         newP.className = 'p-task';
         newButtonDone.className = 'btn-done';
-        newP.setAttribute('contenteditable', 'true');
     }
 
+    newDiv.id = 'drag';
     newDiv1.className = 'div-time';
     newDiv2.className = 'task-btn_btn';
     newTime.className = 'time-task'
@@ -135,101 +161,88 @@ const sortByAlphabetAndData = (time, data, id, done) => {
     newDiv.setAttribute('draggable', 'true')
 
     containerTask.append(newDiv);
-
     newDiv.append(newDiv1, newDiv2);
     newDiv1.prepend(newP, newTime);
-    newDiv2.prepend(newButtonDone, newButtonDel);
 
+    newDiv2.prepend(newButtonDone, newButtonDel);
     ButtonDoneAndDel(newDiv, newDiv2, newP, newButtonDel, newButtonDone);
-    dragAndDrop();
-    ButtonSortAZ()
-    ButtonSortTime()
+    dragAndDrop(newDiv)
+    redactTask(newP)
 }
 
 const ButtonSortAZ = () => {
     buttonSortByAlphabet.addEventListener('click', () => {
-        for (let i = 0; i < toDoList.length; i++) {
-            toDoList.sort((a, b) => {
-                return a.name > b.name ? 1 : -1
-            })
-        }
+        toDoList.sort((a, b) => a.name > b.name ? 1 : -1)
 
-        // Видаляю все на сторінці
         containerTask.innerHTML = ''
 
-        // Створюю нові відсортовані таски
         for (let i = 0; i < toDoList.length; i++) {
             sortByAlphabetAndData(toDoList[i].time, toDoList[i].name, toDoList[i].id, toDoList[i].done)
         }
     })
 }
+ButtonSortAZ()
 
 function ButtonSortTime() {
     buttonSortByTime.addEventListener('click', () => {
-        // Сотрую алфавіту
-        for (let i = 0; i < toDoList.length; i++) {
-            toDoList.sort((a, b) => {
-                return new Date (a.time) - new Date (b.time)
-            })
-        }
-        // Видаляю все на сторінці
+        toDoList.sort((a, b) => a.time > b.time ? 1 : -1)
+
         containerTask.innerHTML = ''
 
-        // Створюю нові відсортовані таски
         for (let i = 0; i < toDoList.length; i++) {
             sortByAlphabetAndData(toDoList[i].time, toDoList[i].name, toDoList[i].id, toDoList[i].done)
         }
-        })
+    })
 }
+ButtonSortTime();
 
-function dragAndDrop(task) {
-        let tasks = task
-         tasks = document.querySelectorAll('.div-task');
-        let dragged = null;
+function dragAndDrop() {
+    tasks = document.querySelectorAll('#drag')
+    dragged = null;
 
-        for (const i of tasks) {
-            i.addEventListener('dragstart', function () {
-                dragged = this;
-                for (const it of tasks) {
-                    if (it !== dragged) it.classList.add('hint')
+    for (let i of tasks) {
+        i.addEventListener('dragstart', function () {
+            dragged = this;
+            for (const it of tasks) {
+                if (it !== dragged) it.classList.add('hint')
+            }
+        })
+
+        i.addEventListener('dragenter', function () {
+            if (this !== dragged) this.classList.add('active')
+        })
+
+        i.addEventListener('dragleave', function () {
+            this.classList.remove('active')
+        })
+
+        i.addEventListener('dragEnd', function () {
+            for (const it of tasks) {
+                it.classList.remove('active')
+            }
+        })
+
+        i.addEventListener('dragover', function (evt) {
+            evt.preventDefault()
+        })
+
+        i.addEventListener('drop', function (evt) {
+            evt.preventDefault()
+            if (this !== dragged) {
+                let all = document.querySelectorAll('#drag'),
+                    draggedpos = 0, droppedpos = 0;
+
+                for (let it = 0; it < all.length; it++) {
+                    if (dragged === all[it]) draggedpos = it
+                    if (this === all[it]) droppedpos = it
                 }
-            })
 
-            i.addEventListener('dragenter', function () {
-                if (this !== dragged) this.classList.add('active')
-            })
-
-            i.addEventListener('dragleave', function () {
-                this.classList.remove('active')
-            })
-
-            i.addEventListener('dragEnd', function () {
-                for (const it of tasks) {
-                    it.classList.remove('hint')
-                    it.classList.remove('active')
+                if (draggedpos < droppedpos) {
+                    this.parentNode.insertBefore(dragged, this.nextSibling)
+                } else {
+                    this.parentNode.insertBefore(dragged, this)
                 }
-            })
-
-            i.addEventListener('dragover', function (evt) {
-                evt.preventDefault()
-            })
-
-            i.addEventListener('drop', function (evt) {
-                evt.preventDefault()
-                if (this !== dragged) {
-                    let all = document.querySelectorAll('.div-task'),
-                        draggedpos = 0, droppedpos = 0;
-                    for (let it = 0; it < all.length; it++) {
-                        if (dragged === all[it]) draggedpos = it
-                        if (this === all[it]) droppedpos = it
-                    }
-
-                    if (draggedpos < droppedpos) {
-                        this.parentNode.insertBefore(dragged, this.nextSibling)
-                    } else {
-                        this.parentNode.insertBefore(dragged, this)
-                    }
-                }
-            })
-        }
+            }
+        })
+    }
 }
--- Homework-5/script/main.js
+++ Homework-5/script/main.js
@@ -10,7 +10,7 @@
 // })
 
 // №2
-// 1. isPrime - Returns true or false, indicating whether the given number is prime.
+// 1. isPrime
 const isPrime = (number) => {
     if (number < 2) return false
     if (number === 2) return true
@@ -22,7 +22,7 @@ const isPrime = (number) => {
 
 isPrime(17)
 
-// 2. factorial - Returns a number that is the factorial of the given number.
+// 2. factorial
 const factorial = (number) => {
     let total = 1;
     for (let i = number; i >= 1; i--) {
@@ -30,9 +30,25 @@ const factorial = (number) => {
     }
     return total
 }
-factorial(6)
+factorial(6);
 
-// 4. isSorted - Returns true or false, indicating whether the given array of numbers is sorted.
+// 3. fib - Returns the nth Fibonacci number.
+// fib(0)                              // 0
+// fib(1)                              // 1
+// fib(10)                             // 55
+// fib(20)                             // 6765
+const fib = (number) => {
+    const total = [0, 1];
+    for (let i = 2; i <= number; i++) {
+        const first = total[i - 1];
+        const second = total[i - 2];
+        total.push(first + second);
+    }
+    return total[number];
+}
+fib(20);
+
+// 4. isSorted
 const isSorted = (numbers) => {
     let res;
     if (numbers.length === 0) res = true
@@ -47,7 +63,7 @@ const isSorted = (numbers) => {
 }
 isSorted([-Infinity, -5, 0, 3, 9])  // true
 
-// 5. reverse - Reverses the given string (yes, using the built in reverse function is cheating).
+// 5. reverse
 const reverse = (str) => {
     str = String(str);
 
@@ -59,7 +75,7 @@ const reverse = (str) => {
 }
 reverse('abcdef')
 
-// 6.indexOf - Implement the indexOf function for arrays.
+// 6.indexOf
 const indexOf = (arrayNum, findNum) => {
     let findIndexNum;
     for (let i = 0; i < arrayNum.length; i++) {
@@ -73,8 +89,66 @@ const indexOf = (arrayNum, findNum) => {
 }
 indexOf([1, 2, 3], 1)
 
-// 7. isPalindrome - Return true or false indicating whether the given string is a plaindrone (case and space insensitive).
-// isPalindrome('')                                // true
-// isPalindrome('abcdcba')                         // true
-// isPalindrome('abcd')                            // false
-// isPalindrome('A man a plan a canal Panama')     // true
+// 7. isPalindrome
+const isPalindrome = (string) => {
+    return string.replace(/ /g,'').
+    split('').
+    reverse().
+    join('').
+    toLowerCase() === string.replace(/ /g,'').toLowerCase()
+}
+isPalindrome('A man a plan a canal Panama')
+
+// 8. missing
+const missing = (array) => {
+    let sumTotal = 0;
+    let sum = 0;
+    array.sort((a, b) => a - b)
+    for (let i = 0; i < array.length; i++) {
+        sumTotal = array[i] * (array[i] + 1) / 2
+        sum += array[i]
+    }
+    return sumTotal - sum === 0 ? undefined : sumTotal - sum
+}
+missing([5, 1, 4, 2]);
+
+// 9. isBalanced - Takes a string and returns true or false indicating whether its curly braces are balanced.
+// isBalanced('}{')                      // false
+// isBalanced('{{}')                     // false
+// isBalanced('{}{}')                    // false
+// isBalanced('foo { bar { baz } boo }') // true
+// isBalanced('foo { bar { baz }')       // false
+// isBalanced('foo { bar } }')           // false
+
+
+const isBalanced = (str) => {
+    str = str.replace(/[a-zа-яё\s]/gi, '')
+    let brc = {
+        '}': '{',
+    };
+
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        if (isClosedBrc(str[i])) {
+            if (brc[str[i]] !== stack.pop()) {
+                return false
+            }
+        } else {
+            stack.push(str[i]);
+        }
+    }
+    return stack.length === 0
+}
+
+const isClosedBrc = (char) => {
+    return ['}'].indexOf(char) > -1
+}
+
+// console.log(isBalanced('}{'));                      // false
+// console.log(isBalanced('{{}'));                     // false
+// console.log(isBalanced('{}{}'));                    // false
+// console.log(isBalanced('foo { bar { baz } boo }')); // true
+// console.log(isBalanced('foo { bar { baz }'));       // false
+// console.log(isBalanced('foo { bar } }'));           // false
+
+// №3 Matrix
